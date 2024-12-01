e4dddddaac8be1b7cd26d246c20d187df47bf836
oliverneut <oliver.neut@gmail.com>
2024-12-01 18:05:31 +0100

    Updated the upload_models script

--- upload_models.py
+++ upload_models.py
@@ -5,14 +5,12 @@ import argparse
 parser = argparse.ArgumentParser(description='Upload embed-attacker model')
 parser.add_argument('--attack_model', type=str, default='gpt2_medium', help='Name of the attacker model')
 parser.add_argument('--embed_model', type=str, default='sent_t5_large', help='Name of embedding model')
-parser.add_argument('--model_name', type=str, default='gpt2_medium', help='Name of the embed-attacker model')
+parser.add_argument('--dataset', type=str, default='personachat', help='Name of dataset: PersonaChat or QNLI')
 args = parser.parse_args()
 
-model_name = args.model_name
 path = f'models/{args.dataset}/{args.attack_model}/attacker_{args.embed_model}'
+model_name = f'{args.dataset}-{args.attack_model}-{args.embed_model}-attacker'
 model = AutoModelForCausalLM.from_pretrained(path)
-tokenizer = AutoTokenizer.from_pretrained(path)
 
 # Upload the model and tokenizer to Hugging Face
 model.push_to_hub(model_name)
-tokenizer.push_to_hub(model_name)
